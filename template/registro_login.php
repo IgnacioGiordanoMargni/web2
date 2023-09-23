3a0e9cd0f186bcf39a560cf57966e82159cfc2e9
Matias Morcillo <143836035+MatiasMor64@users.noreply.github.com>
2023-09-23 16:12:49 -0300

    v4
    se agregaron detalles visuales a la pagina de Inicio de Sesión/ Registro

--- template/registro_login.php
+++ template/registro_login.php
@@ -1,17 +1,23 @@
-<form required action="agregar_usuario" method="POST" class="my-4">
-    <label>Mail</label>
-    <input required name="Mail" type="text">
-    <label>"Nombre_usuario"</label>
-    <input required name="Nombre" type="text" rows=3></input>
-    <label>"Contraseña"</label>
-    <input required name="Contraseña" type="text" rows=3></input>
-    <button type="submit">Registrarse</button>
-</form>
 
-<form required action="logueo" method="POST" class="my-4">
-    <label>"Mail"</label>
-    <input name="Mail" type="text">
-    <label>"Contraseña"</label>
-    <input name="Contraseña" type="text"></input>
-    <button type="submit">Ingresar</button>
-</form>
+<div class="form-box">
+    <h1>Iniciar Sesion/ Registrarse</h1>
+    <div>
+        <form required action="agregar_usuario" method="POST">
+        <label>Mail</label>
+        <input required name="Mail" type="text">
+        <label>Nombre_usuario</label>
+        <input required name="Nombre" type="text" rows=3></input>
+        <label>Contraseña</label>
+        <input required name="Contraseña" type="text" rows=3></input>
+        <button class="boton-form" type="submit">Registrarse</button>
+    </form>
+
+    <form required action="logueo" method="POST">
+        <label>"Mail"</label>
+        <input name="Mail" type="text">
+        <label>"Contraseña"</label>
+        <input name="Contraseña" type="text"></input>
+        <button class="boton-form" type="submit">Ingresar</button>
+    </form>
+</div>
+</div>
