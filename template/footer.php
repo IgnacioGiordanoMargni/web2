fae029e640e169756ff979f7d075a1fe4b6e5210
Matias Morcillo <143836035+MatiasMor64@users.noreply.github.com>
2023-09-25 23:46:15 -0300

    Delete template directory
    Se borran las secciones de codigo de pagina web del branch master, luego se pondran en otra branch

--- template/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-        <div class="copyright">&#169; 2023 Mercado Mitico; By Ignacio Margni & Matias Morcillo</div>
-    </footer>
--- template/form_quitar.php
+++
@@ -1,8 +0,0 @@
-<h1>Suprimir un producto</h1>
-<div class="form-box">
-<form required action="quitar_producto" method="POST" class="my-4">
-    <label>Nombre del producto</label>
-    <input name="Producto" type="text">
-    <button type="submit">Quitar</button>
-</form>
-</div>
